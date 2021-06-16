d2cbd57a262c425600bc0ae177c5d1336735789b
robertluowen <757120778@qq.com>
2021-06-17 02:48:24 +0800

    中文的错误提示信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,8 +1,16 @@
 @extends('layouts.default')
-@section('title',$user->name)
+@section('title', $user->name)
+
 @section('content')
-  <a href="{{ route('users.show', $user->id) }}">
-    <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
-  </a>
-  <h1>{{ $user->name }}</h1>
+  <div class="row">
+    <div class="offset-md-2 col-md-8">
+      <div class="col-md-12">
+        <div class="offset-md-2 col-md-8">
+          <section class="user_info">
+            @include('shared._user_info', ['user' => $user])
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
 @stop
